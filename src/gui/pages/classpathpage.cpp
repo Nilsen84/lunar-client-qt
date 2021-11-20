5454059bfefc1496df5e182567b82ddc912bf6b0
Nilsen84 <56961087+Nilsen84@users.noreply.github.com>
2021-11-21 00:30:04 +0100

    Gave the classpath list functionality

--- src/gui/pages/classpathpage.cpp
+++ src/gui/pages/classpathpage.cpp
@@ -7,6 +7,8 @@
 #include <QVBoxLayout>
 #include <QPushButton>
 #include <QLabel>
+#include <QFileDialog>
+
 
 ClasspathPage::ClasspathPage(QWidget *parent) : ConfigurationPage(parent) {
     QVBoxLayout* mainLayout = new QVBoxLayout();
@@ -14,10 +16,61 @@ ClasspathPage::ClasspathPage(QWidget *parent) : ConfigurationPage(parent) {
 
     {
         classpath = new QListWidget();
+        QFont font;
+        font.setPointSize(14);
+        classpath->setFont(font);
+        auto lunarClasses = new QListWidgetItem("<Lunar Classes>", classpath);
+        lunarClasses->setFlags(Qt::NoItemFlags);
+
+        QPalette palette;
+        palette.setColor(QPalette::Disabled, QPalette::Text, Qt::blue);
+        classpath->setPalette(palette);
+
         QPushButton* add = new QPushButton("Add");
         QPushButton* remove = new QPushButton("Remove");
         QPushButton* moveUp = new QPushButton("Move Up");
         QPushButton* moveDown = new QPushButton("Move Down");
+
+        connect(add, &QPushButton::clicked, [this](){
+            QStringList files = QFileDialog::getOpenFileNames(
+                    nullptr,
+                    "Open JAR",
+                    {},
+                    "JAR Archives (*.jar)"
+                    );
+            for(const QString &str : files){
+                if(!str.isEmpty()){
+                    auto item = new QListWidgetItem(QFileInfo(str).fileName(), classpath);
+                    item->setToolTip(str);
+                    classpath->setCurrentItem(item);
+                }
+            }
+
+        });
+
+        connect(remove, &QPushButton::clicked, [this](){
+            for(auto item : classpath->selectedItems()){
+                delete item;
+            }
+        });
+
+        connect(moveUp, &QPushButton::clicked, [this](){
+            int currentRow = classpath->currentRow();
+            if(currentRow > 0){
+                auto currentItem = classpath->takeItem(currentRow);
+                classpath->insertItem(currentRow - 1, currentItem);
+                classpath->setCurrentRow(currentRow - 1);
+            }
+        });
+
+        connect(moveDown, &QPushButton::clicked, [this](){
+            int currentRow = classpath->currentRow();
+            if(currentRow < classpath->count()-1){
+                auto currentItem = classpath->takeItem(currentRow);
+                classpath->insertItem(currentRow + 1, currentItem);
+                classpath->setCurrentRow(currentRow + 1);
+            }
+        });
 
         QGridLayout* classPathContainer = new QGridLayout();
         classPathContainer->setSpacing(6);
