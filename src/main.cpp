8df277fa3cb4fa7eb0fb899fc000230b9fbf1cb6
Nilsen84 <56961087+Nilsen84@users.noreply.github.com>
2021-11-27 20:51:19 +0100

    Set the application name

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 #include <QApplication>
-#include <QPushButton>
 #include <QStandardPaths>
 
 #include "gui/mainwindow.h"
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+    QApplication::setApplicationName(QStringLiteral("lunar-client-qt"));
     MainWindow mainWindow;
     mainWindow.show();
     return QApplication::exec();
