d67d290d84d24eeab8c84712124c15ca1ec37cbf
awned <65061212+ceilingfans@users.noreply.github.com>
2021-12-09 15:46:58 +0800

    refactor(configurationpage): use header file for constructor definition instead of source file

--- src/gui/pages/configurationpage.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by nils on 11/5/21.
-//
-
-#include "configurationpage.h"
-
-ConfigurationPage::ConfigurationPage(Config& config, QWidget *parent) : config(config), QWidget(parent){
-}
